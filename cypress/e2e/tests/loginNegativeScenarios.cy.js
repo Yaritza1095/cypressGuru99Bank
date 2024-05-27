c191d5fb6738eda261bcae82934c3ed9ca72d5e8
Yaritza1095 <yaritza1095@gmail.com>
2024-05-27 18:33:59 -0400

    remove 1 scenario

--- cypress/e2e/tests/loginNegativeScenarios.cy.js
+++ cypress/e2e/tests/loginNegativeScenarios.cy.js
@@ -13,15 +13,5 @@ describe("Testing login", () => {
     login.getPassword().type(password)
     login.getResetButton().click()
   })
-  it("should display a popup if user or password are invalid and take the user to previous url after clicking ok on the popup", () => {
-    login.getUserID().type(userID)
-    login.getPassword().type(password)
-    login.getLoginButton().click()
-    cy.on('window:confirm', (str) => {
-      expect(str).to.equal('User or Password is not valid')
-      return true
-    })
-      cy.get('button').click()
-    
-})
+  
 })
